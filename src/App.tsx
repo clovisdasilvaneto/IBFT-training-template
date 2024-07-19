989e7a3d3e4347e22bf1b33f2e9000ef58b15f11
MarceloSassi <141765353+MarceloSassi@users.noreply.github.com>
2024-07-19 18:24:34 -0300

    feat:put learning card in the home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import Home from "./pages/Home";
 import LPThemeProvider from "./providers/LPThemeProvider";
-import LearningCard from "./components/LearningCard/LearningCard";
-import Learning from "./containers/Learning";
 
 function App() {
   return (
     <LPThemeProvider>
       <Home />
-
-      <Learning />
     </LPThemeProvider>
   );
 }
